7c1ecb2c3b529bb3bab0981d664d0be55b26ee7e
Anderson <andersonpds14@gmail.com>
2019-11-03 17:17:16 -0300

    removido verificarExistencia

--- src/main/java/com/andersonsouza/testegazin/services/ProdutorService.java
+++ src/main/java/com/andersonsouza/testegazin/services/ProdutorService.java
@@ -1,14 +1,12 @@
 package com.andersonsouza.testegazin.services;
 
 import java.util.List;
-import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.andersonsouza.testegazin.domain.entity.Produtor;
 import com.andersonsouza.testegazin.repository.ProdutorRepository;
-import com.andersonsouza.testegazin.services.exception.ProdutorNaoEncontradoException;
 
 @Service
 public class ProdutorService {
@@ -20,15 +18,6 @@ public class ProdutorService {
 		return (List<Produtor>) produtorRepository.findAll();
 	}
 
-	public Produtor buscar(Long id) {
-		Optional<Produtor> produtor = produtorRepository.findById(id);
-
-		if (produtor.isEmpty()) {
-			throw new ProdutorNaoEncontradoException();
-		}
-		return produtor.get();
-	}
-
 	public void salvar(Produtor produtor) {
 		produtor.setId(null);
 		produtorRepository.save(produtor);
@@ -36,19 +25,14 @@ public class ProdutorService {
 
 	public void atualizar(long id, Produtor produtor) {
 		produtor.setId(id);
-		verificaExistencia(id);
+		// verificaExistencia(id);
 		produtorRepository.save(produtor);
 
 	}
 
 	public void deletar(long id) {
-		verificaExistencia(id);
+		// verificaExistencia(id);
 		produtorRepository.deleteById(id);
 
 	}
-
-	private void verificaExistencia(Long id) {
-		buscar(id);
-	}
-
 }
